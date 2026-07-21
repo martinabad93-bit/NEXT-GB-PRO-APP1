a69006a4427070f5a908a6a974395892c7eda387
martinabad93-bit <martinabad93@hotmail.com>
2026-07-21 12:38:41 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,30 @@
-const CACHE_NAME = 'next-gb-pro-v1';
+const CACHE_NAME = 'next-gb-pro-v1'; //[cite: 3]
 const urlsToCache = [
-  './index.html',
-  './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap'
+  './index.html', //[cite: 3]
+  './manifest.json', //[cite: 3]
+  'icon-192.png', 
+  'icon-512.png', 
+  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap' //[cite: 3]
 ];
 
-// Instalación: Guarda los archivos esenciales en el caché
+// Instalación: Guarda los archivos esenciales en el caché[cite: 3]
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
+    caches.open(CACHE_NAME) //[cite: 3]
       .then(cache => {
-        return cache.addAll(urlsToCache);
+        return cache.addAll(urlsToCache); //[cite: 3]
       })
   );
 });
 
-// Activación: Limpia cachés antiguos si actualizas la versión
+// Activación: Limpia cachés antiguos si actualizas la versión[cite: 3]
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then(cacheNames => { //[cite: 3]
       return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
+        cacheNames.map(cacheName => { //[cite: 3]
+          if (cacheName !== CACHE_NAME) { //[cite: 3]
+            return caches.delete(cacheName); //[cite: 3]
           }
         })
       );
@@ -30,17 +32,17 @@ self.addEventListener('activate', event => {
   );
 });
 
-// Fetch: Sirve la app desde el caché cuando no hay internet
+// Fetch: Sirve la app desde el caché cuando no hay internet[cite: 3]
 self.addEventListener('fetch', event => {
-  // Excluimos las llamadas a APIs externas (VIN y Tasa del Dólar) para que siempre intenten buscar datos frescos
-  if (event.request.url.includes('api.whatsapp.com') || event.request.url.includes('vpic.nhtsa.dot.gov') || event.request.url.includes('open.er-api.com')) {
+  // Excluimos las llamadas a APIs externas (VIN y Tasa del Dólar) para que siempre intenten buscar datos frescos[cite: 3]
+  if (event.request.url.includes('api.whatsapp.com') || event.request.url.includes('vpic.nhtsa.dot.gov') || event.request.url.includes('open.er-api.com')) { //[cite: 3]
     return; 
   }
 
   event.respondWith(
-    caches.match(event.request)
+    caches.match(event.request) //[cite: 3]
       .then(response => {
-        return response || fetch(event.request);
+        return response || fetch(event.request); //[cite: 3]
       })
   );
 });
